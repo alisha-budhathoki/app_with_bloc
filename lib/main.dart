16d7ef13ca5c7c07a7c11b748a06caf814e956ff
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-03-08 14:23:11 +0545

    adding consumer in replacement of bloc builder and bloc listener

--- lib/main.dart
+++ lib/main.dart
@@ -24,47 +24,58 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('You have pushed the button '),
-          BlocBuilder<CounterCubit, CounterState>(builder: (context, state) {
-            if (state.counterValue % 2 == 0) {
-              return Text('even counter value');
-            }
-            else if (state.counterValue == 5) {
-              return Text("number 5");
-            }
-            return Text((state.counterValue < 0)
-                ? 'negative value'
-                : 'COUNTER VALUE ' + state.counterValue.toString());
-          }),
-          SizedBox(
-            height: 24,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  context.bloc<CounterCubit>().decrement();
-                },
-                tooltip: "Decrement",
-                child: Icon(Icons.remove),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  print('Pressed main');
-                  BlocProvider.of<CounterCubit>(context).increment();
-                  // context.bloc<CounterCubit>().increment();
-                },
-                tooltip: "Increment",
-                child: Icon(Icons.add),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You have pushed the button '),
+            BlocConsumer<CounterCubit, CounterState>(
+                builder: (context, state) {
+              if (state.counterValue % 2 == 0) {
+                return Text('even counter value');
+              } else if (state.counterValue == 5) {
+                return Text("number 5");
+              }
+              return Text((state.counterValue < 0)
+                  ? 'negative value'
+                  : 'COUNTER VALUE ' + state.counterValue.toString());
+            },
+                listener: (context, state) {
+                  if (state.wasIncremented == true) {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text("Incremented"),
+                      duration: Duration(milliseconds: 300),
+                    ));
+                  } else if (state.wasIncremented == false) {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text("Decremented"),
+                        duration: Duration(milliseconds: 300)));
+                  }
+                }),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    context.bloc<CounterCubit>().decrement();
+                  },
+                  tooltip: "Decrement",
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    print('Pressed main');
+                    BlocProvider.of<CounterCubit>(context).increment();
+                    // context.bloc<CounterCubit>().increment();
+                  },
+                  tooltip: "Increment",
+                  child: Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
