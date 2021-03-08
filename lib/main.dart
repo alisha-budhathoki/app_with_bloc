b5bb446e56edc3fcdfff1667ed23473578f58dcb
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-03-08 12:42:53 +0545

    flutter_bloc added using bloc provider, bloc builder for simple counter app adding conditions for state changes and noting its response

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/counter_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('You have pushed the button '),
+          BlocBuilder<CounterCubit, CounterState>(builder: (context, state) {
+            if (state.counterValue % 2 == 0) {
+              return Text('even counter value');
+            }
+            else if (state.counterValue == 5) {
+              return Text("number 5");
+            }
+            return Text((state.counterValue < 0)
+                ? 'negative value'
+                : 'COUNTER VALUE ' + state.counterValue.toString());
+          }),
+          SizedBox(
+            height: 24,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  context.bloc<CounterCubit>().decrement();
+                },
+                tooltip: "Decrement",
+                child: Icon(Icons.remove),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  print('Pressed main');
+                  BlocProvider.of<CounterCubit>(context).increment();
+                  // context.bloc<CounterCubit>().increment();
+                },
+                tooltip: "Increment",
+                child: Icon(Icons.add),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
